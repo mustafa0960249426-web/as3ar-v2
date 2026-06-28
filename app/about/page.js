b578ce7996f337addef4de1ba8e2ef60bf1d1900
mustafa0960249426-web <mustafa0960249426@gmail.com>
2026-06-28 23:44:56 +0200

    Create page.js

---
+++ app/about/page.js
@@ -0,0 +1,11 @@
+    export default function About() {
+      return (
+        <div>
+          <h2 className="text-4xl font-bold mb-4">من نحن</h2>
+          <p className="text-lg leading-8 text-white/80">
+            موقع اسعار هو منصتك الاولى لمعرفة احدث الاسعار بطريقة سهلة وسريعة. 
+            هدفنا نوفر ليك كل المعلومات المهمة في مكان واحد بتصميم مريح للعين.
+          </p>
+        </div>
+      );
+    }

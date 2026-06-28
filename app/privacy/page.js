55dfc06a8bddd458216e49922db1a486c90aa772
mustafa0960249426-web <mustafa0960249426@gmail.com>
2026-06-28 23:46:35 +0200

    Create page.js

---
+++ app/privacy/page.js
@@ -0,0 +1,12 @@
+    export default function Privacy() {
+      return (
+        <div>
+          <h2 className="text-4xl font-bold mb-4">سياسة الخصوصية</h2>
+          <p className="text-lg leading-8 text-white/80">
+            نحن في موقع اسعار نحترم خصوصيتك. نحن لا نجمع اي بيانات شخصية. 
+            نستخدم ملفات تعريف الارتباط `Cookies` واعلانات Monetag لتحسين تجربتك فقط.
+            باستخدامك للموقع انت توافق على سياستنا.
+          </p>
+        </div>
+      );
+    }

df2613cd024a9ded7baeb9996878da35d1021d0b
mustafa0960249426-web <mustafa0960249426@gmail.com>
2026-06-28 23:43:05 +0200

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+    export default function Home() {
+      return (
+        <div className="text-center">
+          <h2 className="text-4xl font-bold mb-4">مرحبا بيك في اسعار 🔥</h2>
+          <p className="text-xl text-white/80">احدث الاسعار بتصميم فخم والوان جملية</p>
+        </div>
+      );
+    }
